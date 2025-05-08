8a1ca0c1001616ac2b9d086ce4315c4de7037f8b
Vadim-Andreevskiy <119154255+Vadim-Andreevskiy@users.noreply.github.com>
2025-05-08 14:54:41 +0300

    Create partRoutes.js

---
+++ src/routes/partRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { list, create, get, update, remove } = require('../controllers/partController');
+const authMiddleware = require('../middlewares/authMiddleware');
+router.get('/', authMiddleware, list);
+router.post('/', authMiddleware, create);
+router.get('/:id', authMiddleware, get);
+router.put('/:id', authMiddleware, update);
+router.delete('/:id', authMiddleware, remove);
+module.exports = router;
