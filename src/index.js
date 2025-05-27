5b211facc1ded871980865fbf046c721ecc50745
Vadim-Andreevskiy <119154255+Vadim-Andreevskiy@users.noreply.github.com>
2025-05-27 13:03:09 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const partRoutes = require('./routes/partRoutes');
-const errorHandler = require('./middlewares/errorHandler');
-
-const app = express();
-app.use(express.json());
--- src/models/Part.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-const PartSchema = new Schema({
-  name: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  quantity: { type: Number, default: 0 },
-  seller: { type: Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
-module.exports = model('Part', PartSchema);
